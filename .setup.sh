6b26d632c9b8706ce2861e765cc4e97dd651f78e
kevinkli916 <50645656+kevinkli916@users.noreply.github.com>
2025-02-15 20:43:15 -0800

    removed execute permissions for .setup.sh

--- .setup.sh
+++ .setup.sh
@@ -176,8 +176,6 @@ copy_config() {
   echo "Planting Configuration Files..."
   [ ! -d "$HOME/dotfiles" ] && git clone --bare git@github.com:kevinkli916/dotfiles.git $HOME/dotfiles
   git --git-dir=$HOME/dotfiles/ --work-tree=$HOME checkout -f main
-
-  chmod +x .setup.ch
 }
 
 # Start services
