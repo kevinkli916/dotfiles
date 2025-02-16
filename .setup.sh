380d18511a51ff157ee6bcda861cf3993251d6e7
kevinkli916 <50645656+kevinkli916@users.noreply.github.com>
2025-02-16 01:25:00 -0800

    configure scripting addition for Yabai

--- .setup.sh
+++ .setup.sh
@@ -220,4 +220,7 @@ copy_config
 setup_user_environment
 cleanup
 
+# Configure scripting addition for Yabai
+echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d " " -f 1) $(which yabai) --load-sa" | sudo tee /private/etc/sudoers.d/yabai
+
 start_services
